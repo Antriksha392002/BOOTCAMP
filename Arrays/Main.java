b99908acbaab357441be3df200c031ed6892ce34
Antriksha392002 <116381656+Antriksha392002@users.noreply.github.com>
2026-06-21 23:54:14 +0530

    Update Main.java

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -17,7 +17,7 @@ public class Main{
         
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                System.out.print(arr1[i][j]+" ");
+                System.out.print(arr1[i][j]+"  ");
             }
             System.out.println(" ");
         }
