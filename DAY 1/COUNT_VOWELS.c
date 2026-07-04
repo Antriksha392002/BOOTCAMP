16e8ffe26cdc54a22e68436eb05c69179352e710
Antriksha392002 <116381656+Antriksha392002@users.noreply.github.com>
2026-07-05 00:03:25 +0530

    Update COUNT_VOWELS.c

--- DAY 1/COUNT_VOWELS.c
+++ DAY 1/COUNT_VOWELS.c
@@ -7,7 +7,7 @@ int main(){
     int count=0;
     for(int i=0;i<20;i++){
         if(a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U'||a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'){
-             count++;
+            count++;
         }
     }
 
